532428cb06b519c6bbf5ecb100bfdd6a9fbfa40c
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-02-01 23:13:08 +0100

    Add functions to sign btns

--- script.js
+++ script.js
@@ -24,12 +24,25 @@ const operate = function (a, b, operator) {
 
 const display = function (a, b, operator) {
   const displayText = document.querySelector(".display-text");
-  displayText.textContent = `${a} ${operator} ${b}`;
+  displayText.textContent = a;
 };
+
+const signBtns = document.querySelectorAll(".sign-btn");
+signBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    operator = event.target.textContent;
+  });
+});
 
 const numBtns = document.querySelectorAll(".num-btn");
 numBtns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
-    firstNum = event.target.textContent;
+    if (operator != "") {
+      secNum = event.target.textContent;
+    } else {
+      firstNum = event.target.textContent;
+    }
+
+    display(firstNum, secNum, operator);
   });
 });
